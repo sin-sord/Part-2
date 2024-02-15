7445937c8b6c4404ce71bc594a62a294aa7f7722
DESKTOP-RQH4JFP\Zalya <zalya@sheridancollege.ca>
2024-02-14 20:54:26 -0500

    Task 3 complete: save the Knight’s health

--- Assets/Week 5/Scripts/Healthbar.cs
+++ Assets/Week 5/Scripts/Healthbar.cs
@@ -7,9 +7,22 @@ public class Healthbar : MonoBehaviour
 {
     public Slider slider;
 
+    private void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("HealthStat", slider.value);
+        SendMessage("HealthValue", slider.value, SendMessageOptions.DontRequireReceiver);
+
+    }
+
     public void TakeDamage(float damage)
     {
         slider.value -= damage;
+        HealthSave();
+    }
+
+    public void HealthSave()
+    {
+        PlayerPrefs.SetFloat("HealthStat", slider.value);
     }
 
 }
