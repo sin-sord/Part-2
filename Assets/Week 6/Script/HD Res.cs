9d62a7c005b3f65880f24a09143a39c3fefefc11
DESKTOP-RQH4JFP\Zalya <zalya@sheridancollege.ca>
2024-02-14 22:23:49 -0500

    Task 4a complete: make the UI and change resolution

---
+++ Assets/Week 6/Script/HD Res.cs
@@ -0,0 +1,26 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HDRes : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    public void SetWidth(int newWidth)
+    {
+        width = newWidth;
+    }
+
+    public void SetHeight(int newHeight)
+    {
+        height = newHeight;
+    }
+
+    public void SetRes()
+    {
+        Screen.SetResolution(width, height, false);
+    }
+}
