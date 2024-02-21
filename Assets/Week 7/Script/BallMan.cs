cf8c2850015408a2dbad219d9b57beae02810226
DESKTOP-RQH4JFP\Zalya <zalya@sheridancollege.ca>
2024-02-21 15:16:13 -0500

    Task 2 complete: selecting and moving 1 player at a time

--- Assets/Week 7/Script/BallMan.cs
+++ Assets/Week 7/Script/BallMan.cs
@@ -6,41 +6,43 @@ using UnityEngine;
 
 public class BallMan : MonoBehaviour
 {
-    public bool selected = false;
+    //public bool selected = false;
     public SpriteRenderer changecolor;
+    Rigidbody2D rb;
+    public float speed = 100;
+
 
     void Start()
     {
         changecolor = GetComponent<SpriteRenderer>();  // allows for color change
-    }
-
-
-    void Update()
-    {
-        Selected();  // the function runs everyframe rather than happening one time like a flash.
+        rb = GetComponent<Rigidbody2D>();
+        Selected(false);
     }
 
 
     private void OnMouseDown()
     {
-        selected = true;  // on MouseDown, selected = true
-    }
-    private void OnMouseUp()
-    {
-        selected = false;  // on MouseUp, selected = false
+        Selected(true);
+        Controller.SetCurrentSelection(this);
     }
 
 
-    public void Selected()
+
+    public void Selected(bool selected)
     {
         if (selected == true)  // if the player is selected on MouseDown
         {
             changecolor.color = Color.blue;  // change the color to blue
         }
-        else if (selected == false)  // if the player is deselected on MouseUp
+        else   // if the player is deselected on MouseUp
         {
-            return;  // return to OG color. return; is saying to end the function
+            changecolor.color = Color.red;  // return to OG color. return; is saying to end the function
         }      
+    }
+
+    public void Move(Vector2 direction)
+    {
+        rb.AddForce(direction * speed, ForceMode2D.Impulse);
     }
 }
 
---
+++ Assets/Week 7/Script/Controller.cs
@@ -0,0 +1,57 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Slider chargeSlider;
+    float charge;
+    public float maxCharge;
+    Vector2 direction;
+
+    public static BallMan CurrentSelection { get; private set; } 
+    public static void SetCurrentSelection(BallMan player)
+    {
+        if(CurrentSelection != null)
+        {
+            CurrentSelection.Selected(false);
+        }
+        CurrentSelection = player;
+        CurrentSelection.Selected(true);
+    }
+
+    private void FixedUpdate()
+    {
+        if(direction != Vector2.zero)
+        {
+            CurrentSelection.Move(direction);
+            direction = Vector2.zero;
+        }
+    }
+
+
+    private void Update()
+    {
+        if (CurrentSelection == null) return;
+
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            charge = 0;
+            direction = Vector2.zero;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            charge += Time.deltaTime;  // charge increases
+            charge = Mathf.Clamp(charge, 0, maxCharge);  // setting charges min and max value
+            chargeSlider.value = charge;  // slider value is equal to charge
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            direction = ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)CurrentSelection.transform.position).normalized * charge;
+        }
+    }
+}
